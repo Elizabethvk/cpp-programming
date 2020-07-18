3db23f4b8b2a2015d6231ddbd3983c192a6e0221
Elizabeth <beeetiiis@Gmail.com>
2020-07-18 22:18:11 +0300

    Create chap3_ex2.cpp

---
+++ chapter03/chap3_ex2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "4isla w mili za km mi dai: \n";
+
+	double n;
+	cin >> n;
+	cout << n << " miles are " << n * 1.609 << " km";
+}

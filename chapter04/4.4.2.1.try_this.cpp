cf7368cb3b58324d8eba4e85b1f0bd59dd673b5f
Elizabeth <beeetiiis@Gmail.com>
2020-08-01 16:16:37 +0300

    Create 4.4.2.1.try_this.cpp

---
+++ chapter04/4.4.2.1.try_this.cpp
@@ -0,0 +1,21 @@
+// 4.4.2.1.try_this.cpp
+//
+// The character 'b' is char('a'+1), 'c' is char('a'+2), etc. Use a loop to
+// write out a table of characters with their corresponding integer values:
+//
+//  a   97
+//  b   98 
+//  ...
+//  z   122
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	char c = 'a';
+	cout << "char" << '\t' << "int value\n";
+	while (c <= 'z'){
+		cout << c << '\t' << int(c) << '\n' ;
+		++c;
+	}
+}
